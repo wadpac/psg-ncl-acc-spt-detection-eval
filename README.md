4d7712bd462ae392c8e76ed13fcd4968c139b8e0
Vincent van Hees <vincentvanhees@gmail.com>
2018-01-22 17:43:39 +0100

    various changes to code related to paper

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-some code related to the analyses of psg data from newcastle
+# Code related to the analyses of psg and accelerometer data
+
+##preprocess_acc_data.R
+Uses GGIR to pre-process the accelerometer binary files.
+
+##transferdatatocsvfiles.R
+Script to load the output from GGIR and store relevant data in separate csv files.
+
+##matchfiles.R
+Script to match PSG and accelerometer files and perform the comparison between PSG
+sleep scores and accelerometer derived sleep classifications.
+
+##tib.R
+Function used by matchfiles.R to detect the SPT-window.
--- matchfiles.R
+++ matchfiles.R
@@ -1,13 +1,21 @@
 rm(list=ls())
 graphics.off()
 
+simulate24 = TRUE # whether to simulate 24 hours of data or not
+
+# specific data directories
 pathpsg = "/media/vincent/Exeter/psg_study/cleaned_psg"
 pathacc = "/media/vincent/Exeter/psg_study/cleaned_acc"
+pathparticipantinfo = "/media/vincent/Exeter/psg_study/data participants/participants_diagnosis.csv"
+pathfigures = "/media/vincent/Exeter"
 source("~/GGIR/psg-newcastle/tib.R")
-
+# extract filenames
 namespsg = dir(pathpsg, full.names = TRUE)
 namesacc = dir(pathacc, full.names = TRUE)
 NN = length(namespsg)+length(namesacc)
+#==================================================
+# match psg and accelerometer files
+# initialize data frame
 comp = data.frame(fname=rep(" ",NN),method=rep(" ",NN),time=rep(" ",NN),id=rep(0,NN),stringsAsFactors = FALSE)
 cnt = 1
 for (i in 1:length(namespsg)) {
@@ -27,7 +35,7 @@ for (j in 1:length(namesacc)) {
   cnt = cnt + 1
 }
 
-# find the pairs based on id and date....
+# find the pairs based on id and date:
 compacc = comp[comp$method=="acc",]
 comppsg = comp[comp$method=="psg",]
 compaccR = compacc[grep("right wrist",compacc$fname),]
@@ -43,25 +51,21 @@ psgL$time.psg = as.numeric(as.POSIXlt(psgL$time.x))
 psgL$time.accL = as.numeric(as.POSIXlt(psgL$time.y))
 diffL = (psgL$time.accL - psgL$time.psg) / 3600
 
-# verify by looking at signal...
-
+# now double check matched by visually looking at signal...
+pdf(file=paste0("/media/vincent/Exeter/Supplement_psg_accelerometer.pdf"),width = 7,height = 3.5)
 for (location in c("left","right")) {
   if (location == "right") psgdata = psgR
   if (location == "left") psgdata = psgL
+  # initialize output matrix (this is where we will store the key information needed for the paper)
   output = data.frame(id=rep(0,nrow(psgdata)))
-  output$Sens1 = 0 #output$tib.threshold =  0
-  #output$row = output$error_onset = output$error_wakeup = 0 
-  # output$perc1 = output$perc2 = output$perc3 = output$perc4 = 0
-  
-  pdf(file=paste0("/media/vincent/Exeter/psg_left.pdf"),width = 7,height = 3.5)
-  for (h in 1:nrow(psgdata)) { # c(6,7,9,10,20,21,22,26)){ #1:nrow(psgdata)
-    # print(h)
+  output$Sens1 = 0 #initialize to 0
+  for (h in 1:nrow(psgdata)) { # loop through all psg recordings
     id = as.numeric(unlist(strsplit(unlist(strsplit(psgdata$fname.x[h],"pant"))[2],".cs"))[1])
     PSG = read.csv(psgdata$fname.x[h])
     ACC = read.csv(psgdata$fname.y[h])
     PSG$time = as.POSIXlt(PSG$time,origin="1970-1-1",tz = "Europe/London")
     ACC$timestampPOSIX = as.POSIXlt(ACC$timestampPOSIX,origin="1970-1-1",tz = "Europe/London")
-    # repeat 6 times to match resolution of ACC
+    # repeat psg scores 6 times to match 5 second resolution of ACC vclassifications
     PSG = PSG[rep(seq_len(nrow(PSG)), each=6),]
     starttime = as.numeric(PSG$time[1])
     PSG$time = as.POSIXlt(seq(starttime,starttime+(nrow(PSG)*5)-1,by=5),origin="1970-1-1",tz = "Europe/London")
@@ -69,8 +73,7 @@ for (location in c("left","right")) {
     ACC$timenum = as.numeric(ACC$timestampPOSIX)
     # merge psg and acc data into one dataframe based on timestamps
     psgacc = data.frame()
-    cnt = 0
-    cnt2 = 0
+    cnt = cnt2 = 0
     while(nrow(psgacc) == 0) {
       PSG$timenum = PSG$timenum + 1 # psg times are not rounded to 5 seconds
       psgacc = merge(PSG,ACC,by="timenum")
@@ -89,7 +92,9 @@ for (location in c("left","right")) {
       }
       cnt = cnt + 1
     }
-    # when pairs are clear, look for optimal shift
+    # ========================================================
+    # We know that time blocks are not perfectly synced (just like in all PSG labs around the world)
+    # Therefore, look for optimal shift (a bit similar to http://iopscience.iop.org/article/10.1088/2057-1976/aa985f/meta)
     radi = 120
     shift = rep(0,(2*radi)+1)
     radilist = -radi:radi
@@ -98,18 +103,21 @@ for (location in c("left","right")) {
       shift[g+radi+1] = length(which(psgacc$stagescore[(radi+g+1):(t2-radi+g)] == 0 & psgacc$ENMO[(radi+1):(t2-radi)] > 0.02))
     }
     finalshift = radilist[median(which.min(shift))]
-    # print(paste0(finalshift," ",min(shift)))
-    
     # shift the timestamps for psg
     newtime = psgacc$time[(radi+finalshift+1):(t2-radi+finalshift)]
     psgacc = psgacc[(radi+1):(t2-radi),]
     psgacc$time = newtime
     psgacc = merge(PSG,ACC,by="timenum")
     
-    # apply timeinbed detection and compare
-    
-    # first expand data with dummy data to create realistic conditions
-    blocksize = 12 * 60 * 1.5
+    #=======================================================
+    # apply spt-window detection and compare estimates
+    # first expand data with simulated data to create realistic conditions
+    if (simulate24 == TRUE) {
+      Naddedhours = floor((24 - (nrow(psgacc) / (12*60))) / 2) # Number of hours needed to simulate 24 hours
+    } else {
+      Naddedhours = 1.5 # notice that further on one hour of this is removed to make evaluation specific to recording + 30 minutes
+    }
+    blocksize = 12 * 60 * Naddedhours
     angleblock = rnorm(n = blocksize,mean = 0,sd = 10) + sin((1:blocksize)/pi*0.1) * 40
     psgacc_expand = psgacc[1:blocksize,]
     psgacc_expand$anglez = angleblock
@@ -124,59 +132,85 @@ for (location in c("left","right")) {
     psgacc_expand$stagescore = 0
     psgacc_expand$ENMO = 0.1
     psgacc = rbind(psgacc,psgacc_expand)
-    
-    timeinbed = inbed(psgacc$anglez, k =60, perc = 0.1, inbedthreshold = 15, bedblocksize = 30, outofbedsize = 60, ws3 = 5)
-    
-    # Derive sleep efficiency & AUC for timeinbed classification
-    
-    if (length(timeinbed$lightson) != 0 & length(timeinbed$lightsout) != 0) {
-      # estimate
-      sleepclas = timeinbed$sleep[timeinbed$lightsout:timeinbed$lightson,1]
+    # now use the expanded data for spt window detection.
+    sptwindow = inbed(psgacc$anglez, k =60, perc = 0.1, inbedthreshold = 15, bedblocksize = 30, outofbedsize = 60, ws3 = 5)
+    # detect sleep episodes within the spt window based on previously described algorithm: journals.plos.org/plosone/article?id=10.1371/journal.pone.0142533
+    postch = which(abs(diff(psgacc$anglez)) > 5) #posture change of at least j degrees
+    # count posture changes that happen less than once per ten minutes
+    sdl1 = rep(0,length(psgacc$anglez))
+    q1 = c()
+    if (length(postch) > 1) {
+      q1 = which(diff(postch) > (5*(60/5))) #less than once per i minutes
+    }
+    if (length(q1) > 0) {
+      for (gi in 1:length(q1)) {
+        sdl1[postch[q1[gi]]:postch[q1[gi]+1]] = 1 #periods with no posture change
+      }
+    } else { #possibly a day without wearing
+      if (length(postch) < 10) {  #possibly a day without wearing
+        sdl1[1:length(sdl1)] = 1 #periods with no posture change
+      } else {  #possibly a day with constantly posture changes
+        sdl1[1:length(sdl1)] = 0 #periodsposture change
+      }
+    }
+    sleepepisodes = sdl1
+    # Derive sleep efficiency and various other summary variables & Area under the cruve for spt window classification
+    if (length(sptwindow$lightson) != 0 & length(sptwindow$lightsout) != 0) {
+      # Estimated sleep from the algorithm
+      sleepclas = sptwindow$sleep[sptwindow$lightsout:sptwindow$lightson,1]
       output$est_sle_eff[h] = (length(which(sleepclas == 1)) / length(sleepclas)) * 100
-      output$est_sle_dur[h] = length(sleepclas) / 720
-      # sleepclas_time = psgacc$timenum[timeinbed$lightsout:timeinbed$lightson]
-      tib_est_series = rep(0,nrow(timeinbed$sleep))
-      tib_est_series[timeinbed$lightsout:timeinbed$lightson] = 1
-      sleep_est = data.frame(est = tib_est_series,time=psgacc$timenum)
-      
-      # psg score
+      output$est_PSTdur[h] = length(sleepclas) / 720
+      tib_est_series = rep(0,nrow(sptwindow$sleep))
+      tib_est_series[sptwindow$lightsout:sptwindow$lightson] = 1
+      sleepepisodes[which(tib_est_series != 1)] = 0
+      sleep_est = data.frame(est = tib_est_series,time=psgacc$timenum,sleepepisodes_est = sleepepisodes)
+      output$est_PSTonset[h] = strftime(as.POSIXlt(sleep_est$time[sptwindow$lightsout], origin="1970-1-1", tz="Europe/London")[1], format='%H:%M:%S')
+      output$est_PSTwake[h] = strftime(as.POSIXlt(sleep_est$time[sptwindow$lightson], origin="1970-1-1", tz="Europe/London")[1], format='%H:%M:%S')
+      # True sleep according to PSG
       pss = psgacc$stagescore
-      i234 = which(pss != 0)[1]:length(pss)-match(0,rev(pss))+1
+      psstmp = pss
+      psstmp[psstmp != 0] = 1
+      i234 = which(pss != 0)[1]:(length(pss)-match(1,rev(psstmp))+1)
       tib_true_series = rep(0,length(pss))
       tib_true_series[i234] = 1
       tib_true_series_time = psgacc$timenum
-      # pss_time = psgacc$timenum[i234]
       sleepscore = pss[i234]
       output$true_sle_eff[h] = (length(which(sleepscore != 0)) / length(sleepscore)) * 100
-      output$true_sle_dur[h] = length(sleepscore) / 720
-      # merge
+      output$true_PSTdur[h] = length(sleepscore) / 720
+      output$true_PSTonset[h] = as.character(psgacc$Time..hh.mm.ss.[which(pss != 0)[1]])
+      output$true_PSTwake[h] = as.character(psgacc$Time..hh.mm.ss.[(length(pss)-match(1,rev(psstmp))+1)])
       sleepscore[which(sleepscore != 0)] = 1
-      sleep_psg = data.frame(psg = tib_true_series,time=tib_true_series_time)
+      sleep_psg = data.frame(psg = tib_true_series,time=tib_true_series_time,sleepepisodes_true=psstmp)
+      # merge psg and accelerometer (algorithm) based estimates
       sleep_est_psg = merge(sleep_est,sleep_psg,by="time",all = FALSE)
-      sleep_est_psg = sleep_est_psg[721:(nrow(sleep_est_psg)-720),] # this will remove 1 hour of the dummy data
-      # AUC
+      if (simulate24 == FALSE) {
+        sleep_est_psg = sleep_est_psg[721:(nrow(sleep_est_psg)-720),] # this will remove 1 hour of the dummy data
+      }
+      # Calcultate AUC
       library(pROC)
       roccurve = roc(sleep_est_psg$psg ~ sleep_est_psg$est)
       output$auc[h] = auc(roccurve)
       output$accuracy[h] = length(which(sleep_est_psg$psg == sleep_est_psg$est)) / nrow(sleep_est_psg)
+
     }
+    output$est_sleepdur[h] = length(which(sleep_est_psg$sleepepisodes_est == 1)) / (12*60)
+    output$true_sleepdur[h] = length(which(sleep_est_psg$sleepepisodes_true == 1)) / (12*60)
     #===================================================================
     # plot for visual check
     show = blocksize:(nrow(psgacc)-blocksize+1)
-    # x11()
     par(mfrow=c(2,1),mar=c(2,5,0,2))
     CL =0.9
     CA = 0.8
-    plot(psgacc$time[show],psgacc$stagescore[show],pch=20,ylim=c(-0.5,4.5),type="l",main="",bty="l",axes=FALSE,xlab="",ylab="psg sleep stage", cex.lab = CL)
+    plot(psgacc$time[show],psgacc$stagescore[show],pch=20,ylim=c(-0.5,5),type="l",main="",bty="l",axes=FALSE,xlab="",ylab="psg sleep stage", cex.lab = CL)
     axis(side = 2,at = 0:4,labels = c("Wake","REM","N1","N2","N3"),tick = TRUE,cex.axis=0.6,las=1)
-    text(x = psgacc$time[show][2],y=1,labels = paste0(id),cex = 2,pos = 3,col="green")
-    timeinbedt0t1 = c(timeinbed$lightsout,timeinbed$lightson)
-    plot(psgacc$time[show],psgacc$anglez[show],pch=20,ylim=c(-90,90),type="l", main = "",bty="l",ylab="angle (degrees)",xlab="Time",cex.lab=CL,cex.axis=CA)
-    if (length(timeinbedt0t1) == 2) lines(psgacc$time[timeinbedt0t1],rep(89,2),col="gray",lwd=3,lty=1,lend=2)
+    text(x = psgacc$time[show][1], y=4.5, labels = paste0("id ", id, " ", location, " wrist"), cex = 0.7, pos = 4, col="red")
+    sptwindowt0t1 = c(sptwindow$lightsout,sptwindow$lightson)
+    plot(psgacc$time[show], psgacc$anglez[show], pch=20, ylim=c(-90,90), type="l", main = "",bty="l", ylab="angle (degrees)",xlab="Time",cex.lab=CL,cex.axis=CA)
+    if (length(sptwindowt0t1) == 2) lines(psgacc$time[sptwindowt0t1], rep(89,2), col="blue", lwd=5, lty=1, lend=2)
     
-    psgacc$timeinbed = 0
-    if (length(timeinbedt0t1) == 2) psgacc$timeinbed[timeinbed$lightsout:timeinbed$lightson] = 1
-    psgacc = psgacc[(blocksize+1):(nrow(psgacc)-blocksize),] # remove blocks of dummy data
+    psgacc$sptwindow = 0
+    if (length(sptwindowt0t1) == 2) psgacc$sptwindow[sptwindow$lightsout:sptwindow$lightson] = 1
+    psgacc = psgacc[(blocksize+1):(nrow(psgacc)-blocksize),] # remove blocks of simulated data
     
     #=================================================
     # Define sleep and wake
@@ -187,66 +221,98 @@ for (location in c("left","right")) {
     # Sens 1: Percentage of sleep stage time that was correctly classified as time in bed
     totalsleepstage = length(which(psgacc$stagescore %in% definesleep == TRUE))
     perc_psg_eq_accinbed = round(((totalsleepstage -
-                                     length(which(psgacc$timeinbed == 0 &
+                                     length(which(psgacc$sptwindow == 0 &
                                                     psgacc$stagescore %in% definesleep ==TRUE)))
                                   / totalsleepstage) * 100,digits=2)
     output$Sens1[h] = perc_psg_eq_accinbed
-    # Negative predictive value
-    totaloutofbed = length(which(psgacc$timeinbed == 0))
-    
-    if (totaloutofbed > (12*60)) {
-      output$NPV[h] = round(((totaloutofbed -
-                                length(which(psgacc$timeinbed == 0 &
-                                               psgacc$stagescore %in% definesleep ==TRUE)))
-                             / totaloutofbed) * 100,digits=2)
-    } else {
-      output$NPV[h] = NA
-    }
     output$totaloutofbed[h] = totaloutofbed/12 # in minutes
     output$id[h] = id
-    output$tib.threshold[h] = round(timeinbed$tib.threshold,digits=2)
+    output$tib.threshold[h] = round(sptwindow$tib.threshold,digits=2)
   }
-  dev.off()
-  
-  # prepare some variables for manual investigation
-  npv = output$NP[which(is.na(output$NPV) == FALSE)]
   sen1 = output$Sens1[which(is.na(output$Sens1) == FALSE)]
-  # npv_nb = output$Nblocks_outofbed[which(is.na(output$nega_predic_value) == FALSE)]
   sen1_nb = output$Nblocks_outofbed[which(is.na(output$Sens1) == FALSE)]
   
   print("--------------------------")
   print(output[order(output$Sens1,decreasing = TRUE),])
-  # print(output)
-  diagn = read.csv(file="/media/vincent/Exeter/psg_study/data participants/participants_diagnosis.csv")
-  if (location == "right") output_rigth = merge(output,diagn,by="id")
+  convchartime2number = function(x) {
+    gettime = function(y) {
+      if (is.na(y) == FALSE) {
+        tmp = as.numeric(unlist(strsplit(y,":")))
+        y = tmp[1]+(tmp[2]/60)+(tmp[3]/3600)
+        if (y < 12) y = y + 24
+      }
+      return(y)
+    }
+    x = as.numeric(sapply(X=x,FUN=gettime))
+    return(x)
+  }
+  output$true_PSTonset_nm = convchartime2number(output$true_PSTonset)
+  output$true_PSTwake_nm = convchartime2number(output$true_PSTwake)
+  output$est_PSTonset_nm = convchartime2number(output$est_PSTonset)
+  output$est_PSTwake_nm = convchartime2number(output$est_PSTwake)
+  diagn = read.csv(file=pathparticipantinfo)
+  if (location == "right") output_right = merge(output,diagn,by="id")
   if (location == "left") output_left = merge(output,diagn,by="id")
 }
+dev.off()
 
+print(table(abs(round((output_left$est_PSTdur - output_left$true_PSTdur)-0.5)+0.5)))
+print(table(abs(round((output_right$est_PSTdur - output_right$true_PSTdur)-0.5)+0.5)))
+print(table(abs(round((output_left$est_sleepdur - output_left$true_sleepdur)-0.5)+0.5)))
 
-# Figure 2
-CX = 0.6
-CXM = 0.8
-CXL = 0.7
-YLIM = c(-11,2)
-jpeg(file=paste0("/media/vincent/Exeter/psg_evaluation.jpeg"),width = 7,height = 3.5,units = "in",res=500)
-par(mfrow=c(1,2),mar=c(4,5,3,2))
-meth_diff = output_left$est_sle_dur - output_left$true_sle_dur
+addLoAlines = function(meth_diff){
+  abline(h=mean(meth_diff),lty=1)
+  abline(h=mean(meth_diff)-(1.96*sd(meth_diff)),lty=3)
+  abline(h=mean(meth_diff)+(1.96*sd(meth_diff)),lty=3)
+}
+
+# Figure 3 in paper
+CX = 0.9
+CXM = 0.9
+CXL = 0.9
+CXA = 0.8
+FL = 1 # font label
+CXLegend = 0.7
+YLIM = c(-7,4.1)
+jpeg(file=paste0(pathfigures,"/Figure3.jpeg"),width = 7,height = 7,units = "in",res=500)
+par(mfrow=c(2,2),mar=c(4,5,5,2))
+
+#----------------------------
+meth_diff = output_left$est_PSTdur - output_left$true_PSTdur
+print(range(meth_diff))
 norm = which(output_left$Disorder==0)
 diag = which(output_left$Disorder==1)
-plot(output_left$true_sle_dur[norm],meth_diff[norm],xlab="Sleep period time duration (hours) by PSG",
-     ylab="Difference: Algorithm  - PSG (hrs)",main="left wrist",
-     pch=19,bty="l",ylim=YLIM,xlim=range(output$true_sle_dur),cex=CX,cex.lab=CXL,cex.axis=CX,cex.main=CXM,font.lab=2)
-lines(output_left$true_sle_dur[diag],meth_diff[diag],pch=1,type="p",cex=CX)
-abline(h=0,lty=2)
-legend("topright",legend = c("normal","sleep disorder"),pch=c(19,1),cex=0.5)
+plot(output_left$true_PSTdur[norm],meth_diff[norm],xlab="PSG (hours)",
+     ylab="Algorithm  - PSG (hours)",main="SPT-window duration (left wrist)",
+     pch=19,bty="l",ylim=YLIM,xlim=range(output$true_PSTdur),cex=CX,cex.lab=CXL,cex.axis=CXA,cex.main=CXM,font.lab=FL)
+lines(output_left$true_PSTdur[diag],meth_diff[diag],pch=1,type="p",cex=CX)
+addLoAlines(meth_diff)
 
-meth_diff = output_rigth$est_sle_dur - output_rigth$true_sle_dur
-norm = which(output_rigth$Disorder==0)
-diag = which(output_rigth$Disorder==1)
-plot(output_rigth$true_sle_dur[norm],meth_diff[norm],xlab="PSG: Sleep period time (hrs)",
-     ylab="Difference: Algorithm  - PSG (hrs)",main="right wrist",
-     pch=19,bty="l",ylim=YLIM,xlim=range(output$true_sle_dur),cex=CX,cex.lab=CXL,cex.axis=CX,cex.main=CXM,font.lab=2)
-lines(output_rigth$true_sle_dur[diag],meth_diff[diag],pch=1,type="p",cex=CX)
-abline(h=0,lty=2)
-legend("topright",legend = c("normal","sleep disorder"),pch=c(19,1),cex=0.5)
+meth_diff = output_right$est_PSTdur - output_right$true_PSTdur
+print(range(meth_diff))
+norm = which(output_right$Disorder==0)
+diag = which(output_right$Disorder==1)
+plot(output_right$true_PSTdur[norm],meth_diff[norm],xlab="PSG (hours)",
+     ylab="Algorithm  - PSG (hours)",main="SPT-window duration (right wrist)",
+     pch=19,bty="l",ylim=YLIM,xlim=range(output$true_PSTdur),cex=CX,cex.lab=CXL,cex.axis=CXA,cex.main=CXM,font.lab=FL)
+lines(output_right$true_PSTdur[diag],meth_diff[diag],pch=1,type="p",cex=CX)
+addLoAlines(meth_diff)
+#-------------------------------------
+meth_diff = output_left$est_sleepdur - output_left$true_sleepdur
+norm = which(output_left$Disorder==0)
+diag = which(output_left$Disorder==1)
+plot(output_left$true_sleepdur[norm],meth_diff[norm],xlab="PSG (hours)",
+     ylab="Algorithm - PSG (hours)",main="Sleep duration (left wrist)",
+     pch=19,bty="l",ylim=YLIM,xlim=range(output$true_sleepdur),cex=CX,cex.lab=CXL,cex.axis=CXA,cex.main=CXM,font.lab=FL)
+lines(output_left$true_sleepdur[diag],meth_diff[diag],pch=1,type="p",cex=CX)
+addLoAlines(meth_diff)
+
+meth_diff = output_right$est_sleepdur - output_right$true_sleepdur
+norm = which(output_right$Disorder==0)
+diag = which(output_right$Disorder==1)
+plot(output_right$true_sleepdur[norm],meth_diff[norm],xlab="PSG (hours)",
+     ylab="Algorithm  - PSG (hours)",main="Sleep duration (right wrist)",
+     pch=19,bty="l",ylim=YLIM,xlim=range(output$true_sleepdur),cex=CX,cex.lab=CXL,cex.axis=CXA,cex.main=CXM,font.lab=FL)
+lines(output_right$true_sleepdur[diag],meth_diff[diag],pch=1,type="p",cex=CX)
+addLoAlines(meth_diff)
 dev.off()
